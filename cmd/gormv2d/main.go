171744d46aa21480795336baa091d977c4fb3ce9
fred921110 <vito86077@gmail.com>
2024-07-22 15:07:25 +0800

    更新时， 执行成功后， result.RowsAffected 为0

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"database/sql"
+	"log"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// 获取当前时间
-	now := time.Now()
+	dbData := MysqlConf{
+		Ip:           "127.0.0.1",
+		Pwd:          "12345#lxikm",
+		User:         "root",
+		Port:         3306,
+		Database:     "texas",
+		LoadInterval: 5,
+	}
+	dataDns := dbData.User + ":" + dbData.Pwd + "@tcp(" + dbData.Ip + ":" + strconv.Itoa(dbData.Port) + ")/" + dbData.Database + "?charset=utf8&loc=Local&readTimeout=15s&writeTimeout=15s"
 
-	// 获取当前是当月的第几天
-	dayOfMonth := now.Day()
+	driverName := "mysql"
+	dbs, err := sql.Open(driverName, dataDns)
+	if err != nil {
+		panic(err)
+	}
+	defer dbs.Close()
 
-	// 输出结果
-	fmt.Printf("今天是本月的第 %d 天\n", dayOfMonth)
+	result, err := dbs.Exec("UPDATE players t  SET t.withdraw_sc = t.withdraw_sc + 1 WHERE t.uid = 43368;")
+	if err != nil {
+		log.Fatal("cat not get ", err.Error())
+		return
+	}
+	count, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal("cat not get count", err.Error())
+		return
+	}
+	log.Printf("result %v", count)
+}
+
+type MysqlConf struct {
+	Ip           string `xml:",attr"`
+	Port         int    `xml:",attr"`
+	User         string `xml:",attr"`
+	Pwd          string `xml:",attr"`
+	Database     string `xml:",attr"`
+	LoadInterval int    `xml:",attr"`
 }
