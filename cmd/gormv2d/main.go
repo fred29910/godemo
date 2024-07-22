6baf5cae7dcd6fd6dedbc12721637392abd35dd2
fred921110 <vito86077@gmail.com>
2024-07-22 17:56:52 +0800

    month of day check

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,49 +1,32 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-	"strconv"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	dbData := MysqlConf{
-		Ip:           "127.0.0.1",
-		Pwd:          "12345#lxikm",
-		User:         "root",
-		Port:         3306,
-		Database:     "texas",
-		LoadInterval: 5,
+	if daysInCurrentMonth(2023, 12) != 31 {
+		panic("get error")
 	}
-	dataDns := dbData.User + ":" + dbData.Pwd + "@tcp(" + dbData.Ip + ":" + strconv.Itoa(dbData.Port) + ")/" + dbData.Database + "?charset=utf8&loc=Local&readTimeout=15s&writeTimeout=15s"
-
-	driverName := "mysql"
-	dbs, err := sql.Open(driverName, dataDns)
-	if err != nil {
-		panic(err)
+	if daysInCurrentMonth(2023, 4) != 30 {
+		panic("get error")
 	}
-	defer dbs.Close()
-
-	result, err := dbs.Exec("insert into players(uid, withdraw_sc) value (43, 1) on duplicate key update withdraw_sc = withdraw_sc+1;")
-	if err != nil {
-		log.Fatal("cat not get ", err.Error())
-		return
-	}
-	count, err := result.RowsAffected()
-	if err != nil {
-		log.Fatal("cat not get count", err.Error())
-		return
-	}
-	log.Printf("result %v", count)
 }
 
-type MysqlConf struct {
-	Ip           string `xml:",attr"`
-	Port         int    `xml:",attr"`
-	User         string `xml:",attr"`
-	Pwd          string `xml:",attr"`
-	Database     string `xml:",attr"`
-	LoadInterval int    `xml:",attr"`
+func daysInCurrentMonth(year int, m uint8) int {
+
+	month := time.Month(m)
+	// 计算下个月的第一天
+	if m == 12 {
+		year++
+		//month = time.January
+	}
+
+	firstDayNextMonth := time.Date(year, month+1, 1, 0, 0, 0, 0, time.UTC)
+	// 计算本月的最后一天，即下个月的第一天的前一天
+	lastDayThisMonth := firstDayNextMonth.Add(-24 * time.Hour)
+	// 返回本月的天数
+	return lastDayThisMonth.Day()
 }
