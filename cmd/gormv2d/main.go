3fd058f7c6a0af9a3a9b4e1ab1c57f3c924aa33a
fred921110 <vito86077@gmail.com>
2024-08-07 14:45:52 +0800

    测试gormv2 作偏移计算

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,21 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func main() {
-	if daysInCurrentMonth(2023, 12) != 31 {
-		panic("get error")
+	dns := "root:12345#lxikm@tcp(127.0.0.1:3306)/dbv"
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
 	}
-	if daysInCurrentMonth(2023, 4) != 30 {
-		panic("get error")
+	err = db.AutoMigrate(&LoginRecord{})
+	if err != nil {
+		panic(err)
+	}
+	t := time.Date(2024, time.May, 6, 9, 0, 0, 0, time.UTC)
+	info := &LoginRecord{
+		UID:   130,
+		Year:  t.Year(),
+		Month: int(t.Month()),
+	}
+	UpdateLogin(info, t.Day())
+	updataSql := fmt.Sprintf("UPDATE user_login_record t SET t.days = t.days | (1 << %d) WHERE t.uid = %d   and t.month = %d   and t.year = %d;", t.Day(), info.UID, t.Month(), t.Year())
+	result := db.Exec(updataSql)
+	if result.Error != nil {
+		panic(result)
+	}
+	if result.RowsAffected != 0 {
+		return
+	}
+
+	err = db.Model(info).Clauses(clause.OnConflict{
+		DoUpdates: clause.AssignmentColumns([]string{"update_time", "days"}),
+	}).Create(info).Error
+
+	if err != nil {
+		panic(err)
 	}
 }
 
-func daysInCurrentMonth(year int, m uint8) int {
+type LoginRecord struct {
+	ID         uint   `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	UID        int32  `gorm:"column:uid" json:"uid"`
+	Year       int    `gorm:"column:year" json:"year"`
+	Month      int    `gorm:"column:month" json:"month"`
+	Days       uint32 `gorm:"column:days" json:"days"`
+	UpdateTime int64  `gorm:"column:update_time" json:"update_time"`
+}
+
+func (l LoginRecord) TableName() string {
+	return "user_login_record"
+}
+
+func (b *LoginRecord) CountConsecutiveDays(startDay int) (int, bool) {
+	count := 0
+	if startDay == 0 {
+		totalDay := DaysInCurrentMonth(b.Year, b.Month)
+		startDay = totalDay
+	}
+
+	for day := startDay; day >= 0 && CheckLoginDay(b, day); day-- {
+		count++
+	}
+	return count, startDay == count
+}
+
+func DaysInCurrentMonth(year int, m int) int {
 
 	month := time.Month(m)
 	// 计算下个月的第一天
@@ -29,4 +85,25 @@ func daysInCurrentMonth(year int, m uint8) int {
 	lastDayThisMonth := firstDayNextMonth.Add(-24 * time.Hour)
 	// 返回本月的天数
 	return lastDayThisMonth.Day()
+}
+
+// UpdateLogin 登陆记录
+func UpdateLogin(user *LoginRecord, dayOfMonth int) {
+	if dayOfMonth < 0 || dayOfMonth >= 31 {
+		return
+	}
+	user.Days |= 1 << dayOfMonth
+}
+
+// CheckLoginDay 检测是否登陆
+func CheckLoginDay(user *LoginRecord, dayOfMonth int) bool {
+	if dayOfMonth < 0 || dayOfMonth >= 31 {
+		return false
+	}
+	return user.Days&(1<<dayOfMonth) != 0
+}
+
+type LoginConsecutiveDayes struct {
+	HasLogin         bool `json:"has_login"`
+	ConsecutiveDayes int  `json:"consecutive_dayes"`
 }
