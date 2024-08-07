a1261787220fb3dc42983a0e8f848109c96f8145
fred921110 <vito86077@gmail.com>
2024-08-07 17:20:10 +0800

    show log for sql

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -2,25 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
+	// 设置日志级别为详细模式
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		},
+	)
+
 	dns := "root:12345#lxikm@tcp(127.0.0.1:3306)/dbv"
-	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		panic(err)
 	}
-	err = db.AutoMigrate(&LoginRecord{})
-	if err != nil {
-		panic(err)
-	}
-	t := time.Date(2024, time.June, 12, 9, 0, 0, 0, time.UTC)
+	// err = db.AutoMigrate(&LoginRecord{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+	t := time.Date(2024, time.January, 17, 9, 0, 0, 0, time.UTC)
 	info := &LoginRecord{
 		UID:   130,
 		Year:  t.Year(),
@@ -44,7 +61,7 @@ func main() {
 	if result.Error != nil {
 		panic(result.Error)
 	}
-	fmt.Println(result, info.ID)
+	fmt.Println(result.RowsAffected, info.ID)
 }
 
 type LoginRecord struct {
