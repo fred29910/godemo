071407cf1d58cda5c1afb341c5ef0b2d7e893680
fred921110 <vito86077@gmail.com>
2024-08-12 15:22:50 +0800

    feat: us west time zone test

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func main() {
-	ids := []int{1, 2, 3, 4}
-	sqtest := fmt.Sprintf("sqlxasd in (%#v)", ids)
-	fmt.Println(sqtest)
+	// 获取当前时间
+	now := time.Now()
+
+	usWestTime(now)
+	pvTime := time.Date(2023, time.December, 3, 3, 0, 9, 9, time.UTC)
+	usWestTime(pvTime)
+}
+
+func usWestTime(now time.Time) {
+	// 获取太平洋时区的时间
+	loc, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return
+	}
+
+	// 将当前时间转化为太平洋时区时间
+	pacificTime := now.In(loc)
+
+	zoneName, offset := pacificTime.Zone()
+	// 获取并打印当前时间及其时区信息
+	fmt.Println("Current Time in Pacific Time Zone:", pacificTime)
+	fmt.Println("Time Zone Name:", zoneName)
+	fmt.Println("UTC Offset in Seconds:", offset)
 }
