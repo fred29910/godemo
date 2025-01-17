80428b35ebc1e72ebc3ae2ad6e4d9f6c8a7140a9
fred921110 <vito86077@gmail.com>
2025-01-17 09:29:01 +0800

    测试gorm gen tool 生成代码

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
 	"time"
 
-	"gorm.io/datatypes"
 	"gorm.io/driver/mysql"
+	"gorm.io/gen"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 func main() {
-	// 设置日志级别为详细模式
+
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
@@ -26,84 +25,42 @@ func main() {
 	)
 
 	dns := "root:12345#lxikm@tcp(127.0.0.1:3306)/dbv?parseTime=true&loc=Local"
-	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{
+	gormdb, err := gorm.Open(mysql.Open(dns), &gorm.Config{
 		Logger: newLogger,
 	})
 
 	if err != nil {
 		panic(err)
 	}
-	// err = db.Migrator().AutoMigrate(&PromoCode{})
 
-	// if err != nil {
-	// 	panic(err)
-	// }
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "internal/query",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
 
-	// attr := map[string]interface{}{
-	// 	"test":  "test",
-	// 	"test1": "test1",
-	// 	"test2": "test2",
-	// }
+	// gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(gormdb) // reuse your gorm db
 
-	// atrJson, _ := json.Marshal(attr)
-	// db.Model(&PromoCode{}).Create(&PromoCode{
-	// 	Status: 1,
-	// 	Code:   "test",
-	// 	Info:   "test",
+	// Generate basic type-safe DAO API for struct `model.User` following conventions
 
-	// 	Attributes: datatypes.JSON(atrJson),
-	// })
+	// g.ApplyBasic(
+	// 	// Generate struct `User` based on table `users`
+	// 	g.GenerateModel("users"),
 
-	upsMap := make(map[string]interface{})
+	// 	// Generate struct `Employee` based on table `users`
+	// 	g.GenerateModelAs("users", "Employee"),
 
-	upsMap["attributes"] = nil
+	// 	// Generate struct `User` based on table `users` and generating options
+	// 	g.GenerateModel("users", gen.FieldIgnore("address"), gen.FieldType("id", "int64")),
 
-	err = db.Model(&PromoCode{}).Where("id = ?", 1).Updates(upsMap).Error
-	if err != nil {
-		panic(err)
-	}
-
-	var promoCodes []*PromoCode
-
-	query := db.Model(&PromoCode{})
-
-	query.Where("status = ?", 1)
-	query.Where("info like ?", `%test%`)
-
-	query.Where("created_at < ?", time.Now())
-	err = query.Find(&promoCodes).Error
-
-	if err != nil {
-		panic(err)
-	}
-
-	dataJson, _ := json.Marshal(promoCodes)
-	log.Println(string(dataJson))
+	// 	// Generate struct `Customer` based on table `customer` and generating options
+	// 	// customer table may have a tags column, it can be JSON type, gorm/gen tool can generate for your JSON data type
+	// 	g.GenerateModel("customer", gen.FieldType("tags", "datatypes.JSON")),
+	// )
+	g.ApplyBasic(
+		// Generate structs from all tables of current database
+		g.GenerateAllTable()...,
+	)
+	// Generate the code
+	g.Execute()
 }
-
-type PromoCode struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time      `json:"created_at" gorm:"column:created_at"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"column:updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
-
-	Status int    `json:"status" gorm:"column:status"`
-	Code   string `json:"code" gorm:"column:code"`
-	Info   string `json:"info" gorm:"column:info"`
-
-	Attributes datatypes.JSON `json:"attributes" gorm:"column:attributes"`
-}
-
-const PromoCodeTableName = "promo_code"
-
-func (PromoCode) TableName() string {
-	return PromoCodeTableName
-}
-
-// type PromoCodeDao struct {
-// 	Status    int        `json:"status" gorm:"column:status"`
-// 	Code      string     `json:"code" gorm:"column:code"`
-// 	Info      string     `json:"info" gorm:"column:info"`
-// 	ID        uint       `json:"id" gorm:"column:id"`
-// 	CreatedAt *time.Time `json:"created_at" gorm:"column:created_at"`
-// }
--- go.mod
+++ go.mod
@@ -4,16 +4,23 @@ go 1.21.12
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	gorm.io/driver/mysql v1.5.7
-	gorm.io/gorm v1.25.11
+	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/gorm v1.25.11 // indirect
 )
 
-require gorm.io/datatypes v1.2.4
+require (
+	gorm.io/datatypes v1.2.4 // indirect
+	gorm.io/gen v0.3.26
+)
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
+	gorm.io/hints v1.1.0 // indirect
+	gorm.io/plugin/dbresolver v1.5.0 // indirect
 )
