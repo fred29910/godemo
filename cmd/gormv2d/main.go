c8e88f173af1d4b57c0ffc7c8508e1cfe5459e0a
fred921110 <vito86077@gmail.com>
2025-03-07 14:36:59 +0800

    时间格式化显示

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"encoding/base64"
+	"encoding/json"
 	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func main() {
 
-	base64Data := "jDO1d9RAMP0RcYDdJP/9jOT/Lv/+do0ACR12AAAAADD/CLndGBAEGJi+ByARKMgBMAM45b0HQAFIDlIZCJW+BxoJYm90MTIyNjQ1OMhZSANQAZABAVIbCLm9BxACGglib3QxMjI1NTM4uB1IA1ABkAEBUhwI5b0HEAMaCWJvdDEyMjU5NzjsgAJIA1ABkAEBUhkImL4HEAQaCWJvdDEyMjY0ODi4DVABkAEBUhkIo74HEAUaCWJvdDEyMjY1OTj"
-
-	data, err := base64.StdEncoding.DecodeString(base64Data)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
+	jsonStr := struct {
+		Name       string    `json:"name"`
+		Age        int       `json:"age"`
+		StartTieme time.Time `json:"start_time"`
+	}{
+		Name:       "test",
+		Age:        10,
+		StartTieme: time.Now(),
 	}
-
-	fmt.Printf("%v", data)
+	jsonp, _ := json.Marshal(jsonStr)
+	fmt.Println(string(jsonp))
 }
