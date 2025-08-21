c312ba8c16becf03d524e62fb33208cbd14e388a
fred921110 <vito86077@gmail.com>
2025-08-21 10:09:13 +0800

    feat:通道同步关闭的使用方法

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func main() {
+	// 1. 创建一个 int 类型的 channel
+	ch := make(chan int, 3)
 
-	base64Data := "jDO1d9RAMP0RcYDdJP/9jOT/Lv/+do0ACR12AAAAADD/CLndGBAEGJi+ByARKMgBMAM45b0HQAFIDlIZCJW+BxoJYm90MTIyNjQ1OMhZSANQAZABAVIbCLm9BxACGglib3QxMjI1NTM4uB1IA1ABkAEBUhwI5b0HEAMaCWJvdDEyMjU5NzjsgAJIA1ABkAEBUhkImL4HEAQaCWJvdDEyMjY0ODi4DVABkAEBUhkIo74HEAUaCWJvdDEyMjY1OTj"
+	// 2. 启动一个新的 goroutine
+	go func() {
+		// 向 channel 发送一些数据
+		for i := 1; i <= 3; i++ {
+			fmt.Printf("发送数据: %d\n", i)
+			ch <- i
+			time.Sleep(time.Second)
+		}
+		// 关闭 channel
+		close(ch)
+		fmt.Println("Channel 已关闭")
+	}()
 
-	data, err := base64.StdEncoding.DecodeString(base64Data)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
+	// 3. 在主 goroutine 中，使用 for...range 循环来读取 channel 中的数据
+	fmt.Println("开始从 channel 接收数据...")
+	for v := range ch {
+		fmt.Printf("接收到数据: %v\n", v)
 	}
 
-	fmt.Printf("%v", data)
+	// 4. 验证行为
+	fmt.Println("Range 循环结束，程序退出。")
 }
