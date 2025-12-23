2314b2741fd338c90740c6b28a3cb78cb2c1b765
fred921110 <vito86077@gmail.com>
2025-12-23 10:04:41 +0800

    feat: 尝试使用postgres

--- cmd/gormv2d/main.go
+++ cmd/gormv2d/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
 
-	"gorm.io/driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbSslMode := os.Getenv("DB_SSLMODE")
+	dbTimezone := os.Getenv("DB_TIMEZONE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		dbHost, dbUser, dbPassword, dbName, dbPort, dbSslMode, dbTimezone)
+
 	// 设置日志级别为详细模式
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
@@ -23,8 +41,8 @@ func main() {
 		},
 	)
 
-	dns := "deuv:2134#lxikmv@tcp(127.0.0.1:3306)/dbv?parseTime=true&loc=Local"
-	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{
+	// "deuv:2134#lxikmv@tcp(127.0.0.1:3306)/dbv?parseTime=true&loc=Local"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: newLogger,
 	})
 
@@ -71,12 +89,12 @@ func main() {
 }
 
 type MttAdminFee struct {
-	ID               uint64    `gorm:"column:id;type:bigint(20) unsigned;primaryKey;autoIncrement:true;comment:The squence id" json:"id"`                                             // The squence id
-	PlayerID         uint32    `gorm:"column:player_id;type:int(11) unsigned;not null;uniqueIndex:idx_player_id_mtt_game_id,priority:1;comment:The Player ID" json:"player_id"`       // The Player ID
-	MttGameID        uint32    `gorm:"column:mtt_game_id;type:int(11) unsigned;not null;uniqueIndex:idx_player_id_mtt_game_id,priority:2;comment:The mtt game id" json:"mtt_game_id"` // The mtt game id
-	AdminFee         *float64  `gorm:"column:admin_fee;type:decimal(18,2);not null;default:0.00;comment:The admin fee" json:"admin_fee"`                                              // The admin fee
-	MttGameStartTime time.Time `gorm:"column:mtt_game_start_time;type:datetime;not null;comment:The mtt game start time" json:"mtt_game_start_time"`                                  // The mtt game start time
-	MttGameEndTime   time.Time `gorm:"column:mtt_game_end_time;type:datetime;not null;comment:The mtt game end time" json:"mtt_game_end_time"`                                        // The mtt game end time
-	CreateTime       time.Time `gorm:"column:create_time;type:datetime;not null;comment:The event create time" json:"create_time"`                                                    // The event create time
-	UpdateTime       time.Time `gorm:"column:update_time;type:datetime;not null;comment:The event update time" json:"update_time"`                                                    // The event update time
+	ID               uint64    `gorm:"column:id;type:bigserial;primaryKey;autoIncrement:true;comment:The squence id" json:"id"`                                              // The squence id
+	PlayerID         uint32    `gorm:"column:player_id;type:integer;not null;uniqueIndex:idx_player_id_mtt_game_id,priority:1;comment:The Player ID" json:"player_id"`       // The Player ID
+	MttGameID        uint32    `gorm:"column:mtt_game_id;type:integer;not null;uniqueIndex:idx_player_id_mtt_game_id,priority:2;comment:The mtt game id" json:"mtt_game_id"` // The mtt game id
+	AdminFee         *float64  `gorm:"column:admin_fee;type:decimal;not null;default:0.00;comment:The admin fee" json:"admin_fee"`                                           // The admin fee
+	MttGameStartTime time.Time `gorm:"column:mtt_game_start_time;type:timestamp;not null;comment:The mtt game start time" json:"mtt_game_start_time"`                        // The mtt game start time
+	MttGameEndTime   time.Time `gorm:"column:mtt_game_end_time;type:timestamp;not null;comment:The mtt game end time" json:"mtt_game_end_time"`                              // The mtt game end time
+	CreateTime       time.Time `gorm:"column:create_time;type:timestamp;not null;comment:The event create time" json:"create_time"`                                          // The event create time
+	UpdateTime       time.Time `gorm:"column:update_time;type:timestamp;not null;comment:The event update time" json:"update_time"`                                          // The event update time
 }
